2c3f9a21e07b03b94149bbab14a692729911bbb9
Tamira2804 <tamira2804@gmail.com>
2023-10-30 15:12:15 +0200

    add search by query

--- controllers/contacts/listContacts.js
+++ controllers/contacts/listContacts.js
@@ -2,14 +2,29 @@ const { Contact } = require("../../models/contactModel");
 
 const listContacts = async (req, res, next) => {
   const { _id: owner } = req.user;
-  const { page = 1, limit = 10 } = req.query;
+  const { page = 1, limit = 10, favorite, name, email } = req.query;
   const skip = (page - 1) * limit;
+  const ownerQuery = { owner };
 
-  const allContacts = await Contact.find({ owner }, "-createdAt -updatedAt", {
-    skip,
-    limit,
-  }).populate("owner", "name email");
-  res.status(200).json(allContacts);
+  if (favorite) {
+    ownerQuery.favorite = favorite === "true";
+  }
+  if (name) {
+    ownerQuery.name = { $regex: name, $options: "i" };
+  }
+  if (email) {
+    ownerQuery.email = { $regex: email, $options: "i" };
+  }
+
+  const filteredContacts = await Contact.find(
+    ownerQuery,
+    "-createdAt -updatedAt",
+    {
+      skip,
+      limit,
+    }
+  ).populate("owner", "name email");
+  res.status(200).json(filteredContacts);
 };
 
 module.exports = listContacts;
